b68fea90bc4d71bfdfbcc69fa26a811bc3e90f2e
William Irsal <immanuelirsal1@gmail.com>
2020-10-01 15:12:34 +0700

    Delete method added

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../Context/GlobalState";
 
 export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
+
+  const { deleteTransaction } = useContext(GlobalContext);
+
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
         {sign}Rp. {Math.abs(transaction.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
     </li>
   );
 };
--- src/Context/GlobalState.jsx
+++ src/Context/GlobalState.jsx
@@ -16,10 +16,19 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  //actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
       }}
     >
       {children}
